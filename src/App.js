22c22a487008ec0cf4ae17afbb99f0380b10aa8a
AzureAD\MayurChawla <mayur.chawla@active.ai>
2021-09-13 15:32:58 +0530

    trying adding row

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
+import Row from "./Row";
+
+const Addrow = function() {
+  console.log("addrow clicked");
+  document.getElementById("table").appendChild(document.getElementById("table-row"));
+}
+
 function App() {
+  
   return (
+    
     <div className="App">
-      Inside App
+      <Row/>
+      <button className="add-row-button" onClick={Addrow}>Add Row</button>
     </div>
   );
 }
---
+++ src/Row.js
@@ -0,0 +1,43 @@
+import './rowstyle.css';
+
+function Row() {
+    return(
+        <div id="table" className="table">
+            <div id="table-row" className="table-row">
+                <div className="table-data table-heading">Heading</div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+                <div className="table-data table-mark"><input type="checkbox"/></div>
+            </div>
+        </div>
+    );
+}
+export default Row;
